20845c9483223385b2c06acb80e502b454d106c9
binhe22 <binhe22@gmail.com>
2016-02-23 22:08:39 +0800

    run ok

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # repo-traffic
-Crawl the repo traffic data and export into json.
+Crawl the github repo traffic data.
+
+## Usage:
+### Command
+```./repo_traffic.py username password repo_name```
+
+###Use in code
+```
+from repo_traffic import RepoTraffic
+username = ""#email
+password = ""
+repo_traffic = RepoTraffic(username, password)
+repo_traffic.cache_repo("pullword")
+print repo_traffic.get_all_data()
+```
--- repo_traffic.py
+++ repo_traffic.py
@@ -11,7 +11,8 @@ headers = {
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'max-age=0',
@@ -35,30 +36,60 @@ class RepoTraffic:
                     'commit' : com_val,
                     'authenticity_token' : token}    
         login_req = s.post(session_url, data = login_data, headers=headers)
+        html_object = bs(login_req.content, "html.parser")
+        self.login = html_object.find("meta", {"name": "octolytics-actor-login"}).attrs["content"]
         self.s = s
+        self.cache = False
 
-    def parse_top_list(self, top_list_data):
-        pass
-
-    def get_data(self, repo_name):
-        clone_data_url = "https://github.com/binhe22/pullword/graphs/clone-activity-data"
-        traffic_data_url = "https://github.com/binhe22/pullword/graphs/clone-activity-data"
-        top_list_url = "https://github.com/binhe22/pullword/graphs/traffic?partial=top_lists"
-
-        top_list = self.s.get(top_list_url, headers=headers)
+    def cache_repo(self, repo_name):
+        clone_data_url = "https://github.com/{}/{}/graphs/clone-activity-data".format(self.login, repo_name)
+        traffic_data_url = "https://github.com/{}/{}/graphs/clone-activity-data".format(self.login, repo_name)
+        top_list_url = "https://github.com/{}/{}/graphs/traffic?partial=top_lists".format(self.login, repo_name)
+        self.top_list_data = self.s.get(top_list_url, headers=headers).content
         headers["Referer"] = "https://github.com/binhe22/pullword/graphs/traffic"
         headers["Accept"] = "application/json"
-        clone_data = self.s.get(clone_data_url, headers=headers)
-        traffic_data = self.s.get(traffic_data_url, headers=headers)
-        clone_data_json = json.loads(clone_data.content)
-        traffic_data_json = json.loads(traffic_data.content)
+        self.clone_data = self.s.get(clone_data_url, headers=headers)
+        self.traffic_data = self.s.get(traffic_data_url, headers=headers)
+        self.cache = True
 
+    def get_reffer_sites(self):
+        top_list_data = bs(self.top_list_data, "html.parser")
+        reffer_sites_tr = top_list_data.find("tbody").findAll("tr")
+        reffer_sites_list = []
+        for i in reffer_sites_tr:
+            reffer_site = []
+            for j in i.findAll("td"):
+                    reffer_site.append(j.getText().strip())
+            reffer_sites_list.append(reffer_site)
+        return reffer_sites_list
 
-        print traffic_data.content
-        print top_list.content
+    def get_popular_content(self):
+        top_list_data = bs(self.top_list_data, "html.parser")
+        popular_content_tr = top_list_data.select(".top-content")[0].find("tbody").findAll("tr")
+        popular_content_list = []
+        for i in popular_content_tr:
+            popular_content = []
+            for j in i.findAll("td"):
+                popular_content_list.append(j.getText().strip())
+        return popular_content_list
 
-        
+    def get_clone_data(self):        
+        return json.loads(self.clone_data.content)["counts"]
 
+    def get_visit_data(self):
+        return json.loads(self.traffic_data.content)["counts"]
+
+    def get_all_data(self, repo_name=None):
+        if repo_name:
+            self.cache_repo(repo_name)
+        if not self.cache:
+            return None
+        data_dict = {}
+        data_dict["viist_data"] = self.get_visit_data()
+        data_dict["clone_data"] = self.get_clone_data()
+        data_dict["reffer_sites"] = self.get_reffer_sites()
+        data_dict["popular_content"] = self.get_popular_content()
+        return data_dict
 
 if __name__ == "__main__":
     inputs = sys.argv    
@@ -73,4 +104,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     repo_name = sys.argv[3]
     repo_traffic = RepoTraffic(username, password)
-    repo_traffic.get_data("pullword")
+    repo_traffic.cache_repo("pullword")
+    print repo_traffic.get_all_data()
+    # print repo_traffic.get_visit_data()
+    # print repo_traffic.get_clone_data()
+    # print repo_traffic.get_reffer_sites()
+    # print repo_traffic.get_popular_content()
